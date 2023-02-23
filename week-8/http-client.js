efe431dfb028d9315f33717032d1ac0f6b894628
Patrick Cuauro <pcuauronava@my365.bellevue.edu>
2023-02-22 22:01:06 -0500

    class created and Json imported from the http link api
    API returned and info retrived, table created and parameters completed

---
+++ week-8/http-client.js
@@ -0,0 +1,27 @@
+/*
+===========================================
+; Title: http-client.js
+; Author: Professor Krasso
+; Date: 02 21 2023
+; Modified by: Patrick Cuauro
+; Description: http-client script
+===========================================
+*/
+export default class HttpClient {
+    async get(url,params) {
+        this.params = "";
+        url = new URL(url);
+        url.search = new URLSearchParams(params);
+        //declared as const to keep the value        
+        const res = await fetch(url.toString(),{
+        method:"GET"          
+        });
+        return res;
+    }  
+}
+
+// export function get(){
+//     let response = fetch('https://openlibrary.org/api/books');
+// let data = response.tostring();
+// return data;
+// }
